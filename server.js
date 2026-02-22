90156af9c37e4783f5456aafa93aa70a15e38db7
PRAVEEN2421 <praveenps20589@gmail.com>
2026-02-23 01:20:11 +0530

    Fix CORS issue for dtoken header on Doctor Dashboard

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "atoken"]
+  allowedHeaders: ["Content-Type", "Authorization", "atoken", "dtoken"]
 }));
 
 app.options("*", cors());
