c03f3f88fe6fb24677ed4ab941c0114552604cb3
PRAVEEN2421 <praveenps20589@gmail.com>
2026-02-23 10:34:30 +0530

    Rename project from Prescripto to MediSlot

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://prescripto-hms-backend.onrender.com"
+    "https://medislot-hms-backend.onrender.com"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
